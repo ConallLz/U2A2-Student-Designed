2f81654f8073a243bc077ac1497edfb21fe9081e
ConallLz <conalllorentz@gmail.com>
2025-03-17 11:42:28 -0400

    comments

--- src/Goals.java
+++ src/Goals.java
@@ -4,11 +4,14 @@
 */
 import becker.robots.*;
 
+// Allows extra features to be added to the Thing class
 public class Goals extends Thing{
     Goals(City city, int street, int avenue){
         super(city, street, avenue);
     }
 
+    // Override the getIntersection method and then call the regular getIntersection method
+    // Necessary because the regular getIntersection method is protected
     @Override
     public Intersection getIntersection(){
         return super.getIntersection();
