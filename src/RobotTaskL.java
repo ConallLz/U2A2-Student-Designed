c66bb8fd9fcbc9dc99aeca39549d486d3c3d92d9
ConallLz <conalllorentz@gmail.com>
2025-03-17 11:49:50 -0400

    move

--- src/RobotTaskL.java
+++ src/RobotTaskL.java
@@ -13,21 +13,28 @@ public class RobotTaskL implements Runnable {
 
 	public void run() {
 		RacerBot rob = new RobL(field, 0, 0, Direction.EAST, 0);
-		rob.move(12);
-		rob.pickThing();
-		rob.turnAround();
-		rob.move(2);
-		rob.turnLeft();
+		rob.turnRight();
 		rob.move(5);
 		rob.pickThing();
-		rob.move(4);
-		rob.turnLeft();
-		rob.move(10);
-		rob.turnRight();
 		rob.move(1);
 		rob.turnLeft();
-		rob.move(5);
+		rob.move(6);
 		rob.turnLeft();
+		rob.move(1);
+		rob.turnLeft();
+		rob.move(1);
+		rob.pickThing();
+		rob.turnAround();
+		rob.move(4);
+		rob.turnLeft();
+		rob.move(4);
+		rob.turnRight();
+		rob.move(11);
+		rob.turnLeft();
+		rob.move(1);
+		rob.turnRight();
+		rob.move(5);
+		rob.turnRight();
 		rob.move(5);
 		rob.pickAllThings();
 	}
